6c319e97257a5a5eb6ef031ba2c384809699118f
Japogaitero <japogaitero@gmail.com>
2021-02-12 13:27:47 +0100

    segundo commit

---
+++ src/factorial/claseprueba.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package factorial;
+
+/**
+ * 
+ * @author Victor Alvarez <japogaitero at gmail.com>
+ */
+public class claseprueba {
+
+}
